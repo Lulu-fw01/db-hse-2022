5512f8ef78875360ca35caa59372bce5c522f2d7
luka <limarkov@edu.hse.ru>
2022-11-26 21:44:09 +0300

    task 5

---
+++ hw-8/scripts/task-5.sql
@@ -0,0 +1,17 @@
+create
+    or replace function GET_JOB_COUNT(
+    emp_id in job_history.employee_id%type,) returns integer
+    language plsql
+as
+$$
+declare
+    jobs_count int;
+begin
+    SELECT DISTINCT count(*)
+    INTO jobs_count
+    FROM job_history
+    WHERE employee_id = emp_id;
+
+    return jobs_count;
+end;
+$$
